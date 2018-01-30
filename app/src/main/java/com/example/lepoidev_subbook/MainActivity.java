f6407c28a59b87208ecd18736266cb72f72b36a1
lepoidev <35352972+lepoidev@users.noreply.github.com>
2018-01-30 16:07:22 -0700

    contraint modifications

--- app/src/main/java/com/example/lepoidev_subbook/MainActivity.java
+++ app/src/main/java/com/example/lepoidev_subbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.lepoidev_subbook;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,17 +9,41 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.content.Context;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    //class variables
+    private static final String FILENAME = "file.sav";
+    //private EditText bodyText;
+    private ListView oldSubs;
+
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //bodyText = (EditText) findViewById(R.id.bodyText);
+        oldSubs = (ListView) findViewById(R.id.oldSubs);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +51,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        String[] subs = loadFromFile();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, subs);
+        oldSubs.setAdapter(adapter);
     }
 
     @Override
@@ -48,5 +80,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String[] loadFromFile() {
+        ArrayList<String> subs = new ArrayList<String>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            String line = in.readLine();
+            while (line != null) {
+                subs.add(line);
+                line = in.readLine();
+            }
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return subs.toArray(new String[subs.size()]);
+    }
+
+    private void saveInFile(String text, Date date) {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, Context.MODE_APPEND);
+            fos.write(new String(date.toString() + " | " + text).getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
